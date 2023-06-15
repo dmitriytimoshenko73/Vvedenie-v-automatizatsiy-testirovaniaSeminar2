d731572deba0f7b8dece854e6d38affe263d2c23
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-06-15 19:32:23 +0300

    -

--- src/main/java/org/max/demo/pattern/builder/TestObjectExample.java
+++ src/main/java/org/max/demo/pattern/builder/TestObjectExample.java
@@ -30,55 +30,27 @@ public class TestObjectExample {
         return stringAttr;
     }
 
-    public void setStringAttr(String stringAttr) {
-        this.stringAttr = stringAttr;
-    }
-
     public Integer getIntegerAttr() {
         return integerAttr;
-    }
-
-    public void setIntegerAttr(Integer integerAttr) {
-        this.integerAttr = integerAttr;
     }
 
     public Double getDoubleAttr() {
         return doubleAttr;
     }
 
-    public void setDoubleAttr(Double doubleAttr) {
-        this.doubleAttr = doubleAttr;
-    }
-
     public Short getShortAttr() {
         return shortAttr;
-    }
-
-    public void setShortAttr(Short shortAttr) {
-        this.shortAttr = shortAttr;
     }
 
     public Boolean getBooleanAttr() {
         return booleanAttr;
     }
 
-    public void setBooleanAttr(Boolean booleanAttr) {
-        this.booleanAttr = booleanAttr;
-    }
-
     public LocalDateTime getLocalDateTimeAttr() {
         return localDateTimeAttr;
     }
 
-    public void setLocalDateTimeAttr(LocalDateTime localDateTimeAttr) {
-        this.localDateTimeAttr = localDateTimeAttr;
-    }
-
     public Random getRandomAttr() {
         return randomAttr;
-    }
-
-    public void setRandomAttr(Random randomAttr) {
-        this.randomAttr = randomAttr;
     }
 }
